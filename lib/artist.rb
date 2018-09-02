0abb7cf99cd515caa027f1abf8f3a62b8b74fba5
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 17:40:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,12 @@ class Artist
   
   def initialize(name)
     @name = name
-
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
   
   def add_song_by_name(song)
