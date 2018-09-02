7db056932c3230395222fd0e2f94d11a415e3305
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 18:25:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,22 +7,26 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@count += 1
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    @@count += 1
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@count += 1
   end
   
+  def songs
+    @@songs
+  end
   def self.song_count
-
+    
   end
   
 end
