45c9b8c8f995442f37d11a72fac239512b2ae0db
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 17:59:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,15 @@ class Artist
   end
   
   def self.song_count
-    @@count
+    song_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
   
 end
