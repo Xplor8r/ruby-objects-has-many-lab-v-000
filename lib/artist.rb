744c5f8504387bf646fe9b1b7c1daf3e623538d2
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 17:25:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,6 +7,11 @@ class Artist
     @songs = []
   end
   
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  
   def add_song_by_name(song)
     song = Song.new(name, genre)
     @songs << song
