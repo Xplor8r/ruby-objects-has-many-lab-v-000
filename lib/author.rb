7330fdbde67876d58a675ec24d3a190835c60ea2
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 18:07:53 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,31 @@
 class Author
-  attr_accessor :name
-  def initialize
+  
+  attr_accessor :name, :posts
+  
+  @@count = 0  
+  
+  def initialize(name)
     @name = name
+    @posts = []
+    @@count += 1
   end
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    song.artist = post
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+  
+  def self.song_count
+    @@count
+  end
+  
 end
